64dfe7f18f0fa3000aa878451ec50c7cd96609bb
ianatskiy <igor.anatsky@waveaccess.ru>
2024-06-30 19:26:58 +0300

    Добавил валидацию запроса

--- controller/src/main/kotlin/com/example/controller/controller/SpeedLimitController.kt
+++ controller/src/main/kotlin/com/example/controller/controller/SpeedLimitController.kt
@@ -2,16 +2,19 @@ package com.example.controller.controller
 
 import com.example.controller.model.ChangeSpeedModel
 import com.example.controller.service.SpeedSettings
+import jakarta.validation.Valid
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+@Validated
 @RestController
 class SpeedLimitController(
     val service: SpeedSettings,
 ) {
     @PostMapping("/change-speed")
-    fun limit(@RequestBody data: ChangeSpeedModel) {
+    fun limit(@Valid @RequestBody data: ChangeSpeedModel) {
         service.modify(data.topic, data.speed)
     }
 }
--- controller/src/main/kotlin/com/example/controller/model/ChangeSpeedModel.kt
+++ controller/src/main/kotlin/com/example/controller/model/ChangeSpeedModel.kt
@@ -1,6 +1,15 @@
 package com.example.controller.model
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+
 data class ChangeSpeedModel(
+
+    @field:NotBlank
     val topic: String,
+
+    @field:Min(3_600)
+    @field:Max(3_600_000_000)
     val speed: Long,
 )
